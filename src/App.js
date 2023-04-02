06d855c456e2bf6f5cba21d38f36563f833cc334
raobaba <shrikishunr7@gmail.com>
2023-04-02 21:08:05 +0530

    connection working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+import socketIO from "socket.io-client";
+const ENDPOINT = 'http://localhost:8000/';
+const socket = socketIO(ENDPOINT,{transports:['websocket']})
 function App() {
+   socket.on('connect',()=>{
+    console.log('New Connection')
+   })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Working</h1>
     </div>
   );
 }
