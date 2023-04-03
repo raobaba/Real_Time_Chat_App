a7814cd7291d185ea2372ff62bde8a2fbfd12250
raobaba <shrikishunr7@gmail.com>
2023-04-03 19:28:34 +0530

    start from start

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import './App.css';
 
-import Join from "./Component/Join/Join.jsx";
-import Chat from './Component/Chat/Chat.jsx';
-import {Routes,Route} from 'react-router-dom';
-
 function App() {
 
   return (
     <div className="App">
-     <Routes>
-       <Route path='/' Component={Join} />
-       <Route path='/chat' Component={Chat} />
-     </Routes>
+      <h1>Live Chat App</h1>
     </div>
   );
 }
