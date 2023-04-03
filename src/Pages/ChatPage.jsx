c6f20506f3c04e1e880a9997d63518577c3b9497
raobaba <shrikishunr7@gmail.com>
2023-04-03 22:59:42 +0530

    fetching api

--- src/Pages/ChatPage.jsx
+++ src/Pages/ChatPage.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
-
+import React,{useEffect} from 'react'
+import axios from 'axios';
 export default function ChatPage() {
+    const fetchChats =async ()=>{
+      const {data} = await axios.get('/api/chats');
+      console.log(data);
+    }
+    useEffect(()=>{
+        fetchChats();
+    },[])
   return (
     <div>
       chat
