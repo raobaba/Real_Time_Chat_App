76dcd61440f60a99d6bd7c03b13c8e339d8bd7b3
raobaba <shrikishunr7@gmail.com>
2023-04-07 16:47:37 +0530

    start from start

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './Pages/HomePage';
-import ChatPage from './Pages/ChatPage';
+
 function App() {
 
   return (
     <div className='App'>
-      <Routes>
-      <Route path='/' Component={HomePage} />
-      <Route path='/chat' Component={ChatPage} />
-    </Routes>
+     Hello
     </div>
   );
 }
