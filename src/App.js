cc90526aa93ade4d0a259b2390140288cf463f7f
raobaba <shrikishunr7@gmail.com>
2023-04-07 18:04:43 +0530

    Register page UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
-
+import {Routes,Route} from 'react-router-dom';
+import Chat from './Pages/Chat';
+import Register from './Pages/Register';
+import Login from './Pages/Login';
 function App() {
 
   return (
     <div className='App'>
-     Hello
+     <Routes>
+       <Route path='/' Component={Chat} />
+       <Route path='/register' Component={Register} />
+       <Route path='/login' Component={Login} />
+       <Route />
+     </Routes>
     </div>
   );
 }
